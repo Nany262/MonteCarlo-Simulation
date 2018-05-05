d4b120c19ab74ca05199edaff40018858e184fe6
Daniela Zuluaga <daniela-zuluaga@utp.edu.co>
2018-05-05 13:05:58 -0500

    Generate random points and save in csv

--- ScriptCSV.py
+++ ScriptCSV.py
@@ -2,6 +2,7 @@ import PyQt4.Qsci
 import processing
 from PyQt4.QtGui import QInputDialog
 import pandas as pd
+import os
 
 canvas = qgis.utils.iface.mapCanvas() 
 allLayers = canvas.layers()
@@ -13,3 +14,15 @@ hora = QInputDialog.getText(None, 'HORA', 'Hora')
 for layer in allLayers:
     if layer.name().find("From") >=0 or layer.name().find("to v") >=0:
         QgsVectorFileWriter.writeAsVectorFormat(layer, r'/home/dani/Desktop/MonteCarlo-Simulation/Substations/'+ zona[0] +'/Puntos Aleatorios/'+ dia[0] +' '+ hora[0] +'/'+ layer.name() +'_'+ hora[0] +'.csv', "utf-8", None, "CSV",layerOptions =['GEOMETRY=AS_XY'])
+
+for layer in allLayers:
+    if layer.name().find("From") >=0:
+        dataf1 = pd.read_csv(r'/home/dani/Desktop/MonteCarlo-Simulation/Substations/'+ zona[0] +'/Puntos Aleatorios/'+ dia[0] +' '+ hora[0] +'/'+ layer.name() +'_'+ hora[0] +'.csv', header=0)
+        dataf2 = pd.read_csv(r'/home/dani/Desktop/MonteCarlo-Simulation/Substations/'+ zona[0] +'/Puntos Aleatorios/'+ dia[0] +' '+ hora[0] +'/to v'+ layer.name()[-1] +'_'+ hora[0] +'.csv', header=0)
+
+        dataf_total = dataf1.merge(dataf2,on='ID')
+        dataf_total.drop(dataf_total.columns[[2,3,6]],axis = 1, inplace=True)
+        dataf_total.to_csv(r'/home/dani/Desktop/MonteCarlo-Simulation/Substations/'+ zona[0] +'/Puntos Aleatorios/'+ dia[0] +' '+ hora[0] +'/'+ layer.name() +'_'+ hora[0] +'.csv', header=["X1","Y1","X2","Y2"], index=False)
+        
+        os.remove( r'/home/dani/Desktop/MonteCarlo-Simulation/Substations/'+ zona[0] +'/Puntos Aleatorios/'+ dia[0] +' '+ hora[0] +'/to v'+ layer.name()[-1] +'_'+ hora[0] +'.csv')
+        
